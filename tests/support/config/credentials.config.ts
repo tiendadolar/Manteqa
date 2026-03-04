f2db104cfcb883cba121457895540b114864199c
DAndresPeralta <danteandresperalta@gmail.com>
2026-03-04 20:44:32 -0300

    [ LOGs ]

--- tests/support/config/credentials.config.ts
+++ tests/support/config/credentials.config.ts
@@ -3,7 +3,7 @@ import logger from '../utils/logger';
 export const getCredentials = (companyId: string) => {
   let secret = `CREDS_${companyId}`;
   let credentials = process.env[secret];
-
+  logger.warn(credentials);
   if (!credentials) {
     logger.warn(`Credentials for company: ${companyId} not configured`);
     credentials = '';
