e45946a40b78df067eff767b3f0cf413f26a70d9
DAndresPeralta <danteandresperalta@gmail.com>
2026-03-04 21:16:30 -0300

    [ LOGs ]

--- tests/support/config/credentials.config.ts
+++ tests/support/config/credentials.config.ts
@@ -1,9 +1,21 @@
 import logger from '../utils/logger';
 
 export const getCredentials = (companyId: string) => {
-  logger.warn(companyId.trim().toUpperCase());
-  let normalized = companyId.trim().toUpperCase();
-  let secret = `CREDS_${normalized}`;
+  let secret = companyId;
+  logger.warn(secret);
+
+  const creds = process.env.CREDENTIALS_MAP;
+
+  if (creds) {
+    const map = JSON.parse(creds);
+    const credentials = map[secret];
+    if (!credentials) {
+      logger.warn(`Credentials for company: ${companyId} not configured in CREDENTIALS_MAP`);
+      return '';
+    }
+  }
+
+  // let secret = `CREDS_${normalized}`;
   let credentials = process.env[secret];
   logger.warn(credentials);
   if (!credentials) {
