ce1ba00a085fdb5e12f7912ff589d3f4200adbdd
DAndresPeralta <danteandresperalta@gmail.com>
2026-04-16 16:35:50 -0300

    [ UPDATE ] - Log TrackId

--- tests/hooks/hooks.ts
+++ tests/hooks/hooks.ts
@@ -9,4 +9,18 @@ Before(function (scenario: any) {
 After(function () {
   CustomWorld.clearStoreData(true);
   logger.info('🧈 Isolating stage...');
+
+  const now = new Date();
+
+  const timezone = -3;
+  const argentina = new Date(now.getTime() + timezone * 60 * 60 * 1000);
+
+  const isoString = argentina.toISOString();
+
+  const date = isoString.slice(0, 10);
+  const time = isoString.slice(11, 23);
+
+  const timestamp = `${date} ${time}`;
+
+  console.log(`Track: ${timestamp}`);
 });
